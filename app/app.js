9d97241e7a7c0338b63ee22429ac6341a9544372
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-27 12:10:04 +0100

    feat: Mount admin API routes under /api/v1/admins
    - Mounted the `adminRouter` under the `/api/v1/admins` path using `app.use`.
    - Establishes a base URL for all admin API functionalities (details likely defined in adminRouter).

--- app/app.js
+++ app/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const adminRouter = require("../routes/staff/adminRouter")
 
 const app = express();
+
+// ----- MIDDLEWARES -----
+app.use(morgan('dev'));
+
+
+// ** Routes ** //
+
+// Router now been used as a middleware
+app.use('/api/v1/admins', adminRouter); // app.use accept any http methods, HTTP request that starts with /api/v1/admins will trigger the middleware function adminRouter.meaning that any incoming HTTP request with a path starting with /api/v1/admins will be passed to the adminRouter middleware for further processing. This could include handling specific CRUD (Create, Read, Update, Delete) operations related to admins, authentication, authorization, or any other logic specific to admin-related functionality in your application.
+
+
+
 
 
 module.exports = app
