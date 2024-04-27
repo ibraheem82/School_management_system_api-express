d21fe86d3b8dc0322b7efde8ea6601a9d9b7cf35
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-27 16:01:03 +0100

    comments was added for better understanding...

--- model/Staff/Admin.js
+++ model/Staff/Admin.js
@@ -26,6 +26,21 @@ const adminSchema = new mongoose.Schema(
 );
 
 // Hash Password
+/*
+ --The code is placed within a Mongoose schema's pre('save') middleware function. This function is executed before a new document (representing a user or admin in this case) is saved to the database.
+ -- Checking for Password Modification:
+
+ --Inside the middleware, the code first checks if the password field of the current document has been modified using this.isModified('password').
+ --If the password hasn't been changed (meaning it's likely an existing user logging in), the code exits the function using next() without hashing again. This optimization avoids unnecessary processing.
+Salting (if Password Modified):
+
+ --If the password has been modified (indicating a new user or password change), the code proceeds with hashing.
+
+ --It uses the bcrypt.genSalt(12) function to generate a random string called a "salt." This salt is crucial for enhancing password security. It prevents attackers from pre-computing rainbow tables (a technique to crack hashed passwords) because each password is hashed with a unique salt.
+
+
+
+*/
 adminSchema.pre('save', async function (next){
   if(!this.isModified('password')){
     next();
