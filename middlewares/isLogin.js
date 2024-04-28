8a49340714a4ea57e68c6d1a55865199dff5ade5
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-28 11:31:34 +0100

    feat: Implement JWT authentication middleware with token verification
    - Introduced `isLogin` middleware to verify user authorization using JWTs.
    - Extracts the token from the Authorization header ('Bearer <token>').
    - Utilizes `verifyToken` (from `../utils/verifyToken`) to validate the token.
    - Sets `req.userAuth` with the verified user data if the token is valid.
    - Allows requests to proceed if the token is valid (authenticated user).
    - Creates a new `Error` object with a message indicating invalid/expired tokens for unauthorized requests.
    - Utilizes `next(err)` to forward the error to a subsequent error handler for appropriate response.

--- middlewares/isLogin.js
+++ middlewares/isLogin.js
@@ -1,12 +1,21 @@
+const verifyToken = require("../utils/verifyToken");
+
 // ** checking if a user is logged in or not.
 const isLogin = (req, res, next)=> {
-    const isLogin = req.userAuth; // this is getting the already authenticated user. from the user in the adminController.
-    if(isLogin){
-        next()
+    // get token from header
+    const headerObj = req.headers;
+    const token = headerObj.authorization.split(" ")[1];
+    const verifiedToken = verifyToken(token); // verify the token that is coming from the header.
+
+    if (verifiedToken){
+        // save the user into the req.obj
+        req.userAuth = verifiedToken;
+        next();
     } else{
-        const err = new Error('You are not login')
+        const err = new Error('Token expired/invalid');
         next(err);
     }
+
 };
 
 module.exports = isLogin;
