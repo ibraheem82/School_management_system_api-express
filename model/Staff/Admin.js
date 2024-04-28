516b822e89bfc5ea1dc3470e3eaafd20fcc982c8
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-04-28 12:37:45 +0100

    feat: Define relationships for Admin model with academic terms, years, levels, teachers, and students
    - Added fields to the Admin model schema:
        - `academicTerms`: An array of ObjectIds referencing related `AcademicTerm` documents.
        - `academicYears`: An array of ObjectIds referencing related `AcademicYear` documents.
        - `classLevels`: An array of ObjectIds referencing related `ClassLevel` documents.
        - `teacher`: An ObjectId referencing a related `Teacher` document (one-to-one relationship).
        - `students`: An array of ObjectIds referencing related `Student` documents (one-to-many relationship).
    
    **Explanation:**
    
    - Establishes relationships between the Admin model and other models for comprehensive course data management.

--- model/Staff/Admin.js
+++ model/Staff/Admin.js
@@ -19,6 +19,30 @@ const adminSchema = new mongoose.Schema(
       type: String,
       default: "admin",
     },
+    academicTerms:[{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "AcademicTerm",
+    }],
+
+    academicYears:[{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "AcademicYear",
+    }],
+
+    classLevels:[{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ClassLevel",
+    }],
+
+    teacher:[{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Teacher",
+    }],
+
+    students:[{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+    }],
   },
   {
     timestamps: true,
