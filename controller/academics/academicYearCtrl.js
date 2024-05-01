3a0d2baa87a818c38c5ee4cd6de8040166d51ab5
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-05-01 06:06:56 +0100

    feat: Implement API endpoint for creating academic years
    - Introduced `createAcademicYear` controller for handling academic year creation requests.
    - Validates if the provided name already exists in the `AcademicYear` model before creation.
    - Throws an error if a duplicate name is found.
    - Creates a new academic year document with:
        - `name`: Provided name from the request body.
        - `fromYear`: Provided starting year from the request body.
        - `toYear`: Provided ending year from the request body.
        - `createdBy`: User ID extracted from `req.userAuth._id` (assuming user authentication).
    - Returns a success response with status code 201 (`Created`) upon successful creation.
    - Response includes a success message and the newly created academic year data.

---
+++ controller/academics/academicYearCtrl.js
@@ -0,0 +1,23 @@
+const AsyncHandler = require("express-async-handler");
+const AcademicYear = require("../../model/Academic/AcademicYear")
+exports.createAcademicYear = AsyncHandler(async (req, res) => {
+    const {name, fromYear, toYear}  = req.body
+
+    const academicYear = await AcademicYear.findOne({name});
+
+    if (academicYear) {
+        throw new Error("Academic year already exists");
+    }
+
+    const academicYearCreated = await AcademicYear.create({
+        name,
+        fromYear,
+        toYear,
+        createdBy : req.userAuth._id,
+    });
+    res.status(201).json({
+        status: 'Success',
+        message: "Academic year created successfully",
+        data : academicYearCreated
+    });
+});
