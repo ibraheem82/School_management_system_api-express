768cd1b7bffdc071829d910f606a141835a63839
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-05-01 17:40:44 +0100

    feat: Optimize admin data retrieval for academic year association
    - Enhanced logic to retrieve the current admin user efficiently for academic year association.
    - Uses `findById(req.userAuth._id)` to fetch the admin document by ID.
    - Applies field selection with `.select('-password -createdAt -updatedAt')` to exclude unnecessary data (password, timestamps).
    - Populates the `academicYears` field using `.populate('academicYears')` to retrieve associated academic years.
    - Improves performance by fetching only required data and reduces the size of the response.

--- controller/staff/adminController.js
+++ controller/staff/adminController.js
@@ -79,7 +79,7 @@ exports.getAdminsCtrl = AsyncHandler( async (req, res) => {
 // @route GET /api/v1/admins/:id
 // @access Private
 exports.getAdminProfileCtrl = AsyncHandler( async (req, res) => {
-    const admin = await Admin.findById(req.userAuth._id).select('-password -createdAt -updatedAt')
+    const admin = await Admin.findById(req.userAuth._id).select('-password -createdAt -updatedAt').populate("academicYears");
     if(!admin){
         throw new Error("Admin not found")
     } else{
