f428c7a9ff5752544e57ee6de4e02ef083d95448
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-05-12 19:55:48 +0100

    general route was added for the yearGroup routes...

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ const classLevelRouter = require("../routes/academics/classLevel");
 const programRouter = require("../routes/academics/program");
 const adminRouter = require("../routes/staff/adminRouter");
 const subjectRouter = require("../routes/academics/subject");
+const yearGroupRouter = require("../routes/academics/yearGroup");
 
 
 
@@ -28,6 +29,7 @@ app.use('/api/v1/academic-terms', academicTermRouter);
 app.use('/api/v1/class-levels', classLevelRouter);
 app.use('/api/v1/programs', programRouter);
 app.use('/api/v1/subjects', subjectRouter);
+app.use('/api/v1/year-groups', yearGroupRouter);
 
 // ** -- Error Middlewares --
 app.use(notFoundErr)
