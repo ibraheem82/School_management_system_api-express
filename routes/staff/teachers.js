dfc665957d09dd8052cf566dd862a8bb149d8728
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-05-17 04:43:31 +0100

    Add Protected Route for Teachers to Get Their Profile
    * Defines a new GET route `/profile` on the teacher router.
    * Enforces teacher login (`isTeacherLogin`) and role (`isTeacher`) for access.
    * Uses `getTeacherProfile` controller function to retrieve the logged-in teacher's profile.

--- routes/staff/teachers.js
+++ routes/staff/teachers.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { adminRegisterTeacher, loginTeacher, getAllTeachersAdmin, getTeacherByAdmin } = require("../../controller/staff/teachersControllers");
+const { adminRegisterTeacher, loginTeacher, getAllTeachersAdmin, getTeacherByAdmin, getTeacherProfile } = require("../../controller/staff/teachersControllers");
 const isAdmin = require("../../middlewares/isAdmin");
 const isLogin = require("../../middlewares/isLogin");
+const isTeacherLogin = require("../../middlewares/isTeacherLogin");
+const isTeacher = require("../../middlewares/isteacher");
 
 const teacherRouter = express.Router()
 
@@ -9,6 +11,8 @@ const teacherRouter = express.Router()
 teacherRouter.post("/admin/register", isLogin, isAdmin, adminRegisterTeacher);
 teacherRouter.post("/login", loginTeacher);
 teacherRouter.get("/admin", isLogin, isAdmin, getAllTeachersAdmin);
+teacherRouter.get("/profile", isTeacherLogin, isTeacher, getTeacherProfile);
+// ! Always put route with ID's Below other routes
 teacherRouter.get("/:teacherID/admin", isLogin, isAdmin, getTeacherByAdmin);
 
 module.exports = teacherRouter;
