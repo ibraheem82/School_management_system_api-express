f81abf6090960dc4f1a141c52646cbad66cfc16b
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-05-17 04:32:10 +0100

    Add Route for Admins to Get Single Teacher by ID (/admin)
    * Defines a new GET route `/:teacherID/admin` on the teacher router.
    * Enforces authentication (`isLogin`) and admin privileges (`isAdmin`) for access.
    * Uses `getTeacherByAdmin` controller function to retrieve a specific teacher.

--- routes/staff/teachers.js
+++ routes/staff/teachers.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { adminRegisterTeacher, loginTeacher, getAllTeachersAdmin } = require("../../controller/staff/teachersControllers");
+const { adminRegisterTeacher, loginTeacher, getAllTeachersAdmin, getTeacherByAdmin } = require("../../controller/staff/teachersControllers");
 const isAdmin = require("../../middlewares/isAdmin");
 const isLogin = require("../../middlewares/isLogin");
 
@@ -9,5 +9,6 @@ const teacherRouter = express.Router()
 teacherRouter.post("/admin/register", isLogin, isAdmin, adminRegisterTeacher);
 teacherRouter.post("/login", loginTeacher);
 teacherRouter.get("/admin", isLogin, isAdmin, getAllTeachersAdmin);
+teacherRouter.get("/:teacherID/admin", isLogin, isAdmin, getTeacherByAdmin);
 
 module.exports = teacherRouter;
