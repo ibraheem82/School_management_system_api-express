ce8b25d54a700ab1a756d6294f528494a543f31f
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-05-18 10:17:18 +0100

    comment was removed

--- model/Staff/Admin.js
+++ model/Staff/Admin.js
@@ -59,13 +59,6 @@ const adminSchema = new mongoose.Schema(
 
 
 
-// Verify Password
-// what ever that is been put on the method will be available on the instance of the admin.
-// * definde a method named verifyPassword that will be available on instances (documents) created from this schema.
-// takes a single argument named enteredPassword
-// * Inside the method, the code uses bcrypt.compare(enteredPassword, this.password) to compare the enteredPassword with the hashed password stored in the current document's password field (this.password).
-// bcrypt.compare is a function from the bcrypt library that performs the secure comparison of a plain text password with a hashed password. It returns a Promise that resolves to true if the passwords match and false otherwise.
-
 
 //model
 const Admin = mongoose.model("Admin", adminSchema);
