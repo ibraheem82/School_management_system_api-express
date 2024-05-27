a500141c426790f08bb1b291b8aeb79496bfcc51
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-05-27 16:50:17 +0100

    feat: Add student login route
    - Set up POST /login route for student login.
    - Connect `loginStudent` controller to handle the login process.

--- routes/staff/student.js
+++ routes/staff/student.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { adminRegisterStudent } = require("../../controller/students/studentsCtrl");
+const { adminRegisterStudent, loginStudent } = require("../../controller/students/studentsCtrl");
 const isAdmin = require("../../middlewares/isAdmin");
 const isLogin = require("../../middlewares/isLogin");
 const studentRouter = express.Router()
 
 
 studentRouter.post("/admin/register", isLogin, isAdmin, adminRegisterStudent);
+studentRouter.post("/login", loginStudent);
 
 
 module.exports = studentRouter;
