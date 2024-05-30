89d96a453a65d59482de207702eab87fadb8cef8
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-05-30 19:10:29 +0100

    feat: Add route for teachers to fetch all questions
    - Set up GET / route for retrieving all questions.
    - Apply `isTeacherLogin` middleware to ensure the user is logged in.
    - Apply `isTeacher` middleware to ensure the user has teacher privileges.
    - Connect `getQuestions` controller to handle fetching questions.

--- routes/academics/questionRoutes.js
+++ routes/academics/questionRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { createQuestion } = require("../../controller/academics/questionsCtrl");
+const { createQuestion, getQuestions } = require("../../controller/academics/questionsCtrl");
 const isTeacher = require("../../middlewares/isTeacher");
 const isTeacherLogin = require("../../middlewares/isTeacherLogin");
 
 const questionsRouter = express.Router();
 
 
+questionsRouter.get("/", isTeacherLogin, isTeacher, getQuestions)
 questionsRouter.post("/:examID", isTeacherLogin, isTeacher, createQuestion)
 
 module.exports = questionsRouter;
