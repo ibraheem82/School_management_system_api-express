44c6b9c3708915e583c636ebcf3738807a693cfe
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-05-30 15:45:05 +0100

    isTeacher  imported...

--- routes/academics/examRoutes.js
+++ routes/academics/examRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { createExam, getExams, getExam, updateExam } = require("../../controller/academics/examsCtrl");
-const isTeacher = require("../../middlewares/isteacher");
 const isTeacherLogin = require("../../middlewares/isTeacherLogin");
+const isTeacher = require("../../middlewares/isTeacher");
 
 
 
