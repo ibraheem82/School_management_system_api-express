898068b8532ffa79b808713b912a560032fa8865
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-05-30 19:33:14 +0100

    feat: Implement route to update a question
    - Add `updateQuestion` function to handle updating a question.
    - Secure route with private access for teachers only.
    - Check if the updated question already exists to prevent duplicates.
    - Update question details including options and correct answer.
    - Return success response with the updated question data.

--- routes/academics/questionRoutes.js
+++ routes/academics/questionRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createQuestion, getQuestions, getQuestion } = require("../../controller/academics/questionsCtrl");
+const { createQuestion, getQuestions, getQuestion, updateQuestion } = require("../../controller/academics/questionsCtrl");
 const isTeacher = require("../../middlewares/isTeacher");
 const isTeacherLogin = require("../../middlewares/isTeacherLogin");
 
@@ -9,5 +9,6 @@ const questionsRouter = express.Router();
 questionsRouter.get("/", isTeacherLogin, isTeacher, getQuestions)
 questionsRouter.get("/:id", isTeacherLogin, isTeacher, getQuestion)
 questionsRouter.post("/:examID", isTeacherLogin, isTeacher, createQuestion)
+questionsRouter.put("/:id", isTeacherLogin, isTeacher, updateQuestion)
 
 module.exports = questionsRouter;
